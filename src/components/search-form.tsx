8bcd110f1d4995bfe2f7b36d2f58aed47809cba4
KauaGabrielMG <zombotron@rede.ulbra.br>
2025-04-03 02:33:11 -0300

    fix: corrige fechamento de componente SidebarGroupContent no formulário de busca

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -4,7 +4,7 @@ export function SearchForm({ ...props }: React.ComponentProps<'form'>) {
   return (
     <form {...props}>
       <SidebarGroup className="py-0">
-        <SidebarGroupContent className="relative"></SidebarGroupContent>
+        <SidebarGroupContent className="relative" />
       </SidebarGroup>
     </form>
   );
